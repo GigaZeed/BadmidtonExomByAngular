d0caaf25b87302bd3792382d98e3289a1f0bad0f
Tenshinhan10 <kornphat10@gmail.com>
2023-04-10 16:59:15 +0700

    AllFront

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,29 @@ import { DetailsComponent } from './components/details/details.component';
 import { ListsComponent } from './components/lists/lists.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PromotionComponent } from './components/promotion/promotion.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddComponent,
     DetailsComponent,
-    ListsComponent
+    ListsComponent,
+    NavbarComponent,
+    FooterComponent,
+    PromotionComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
