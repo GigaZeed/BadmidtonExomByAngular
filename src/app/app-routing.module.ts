5e793f9ee50952fad05133f874f966464ecbb370
Tenshinhan10 <kornphat10@gmail.com>
2023-04-11 01:11:31 +0700

    AllFront2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,6 @@ const routes: Routes = [
   {path: 'home',component:HomeComponent},
   {path: 'login',component:LoginComponent},
   {path: 'register',component:RegisterComponent}
-  
 ];
 
 @NgModule({
